eb3d6813c4e2dc8bc75b1877d1a5ef80faa9e106
Elena Lucher <35640122+helentsg@users.noreply.github.com>
2024-03-12 13:25:50 +0500

    Initial Commit

---
+++ SerpApiTestingTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SerpApiTestingTask
+//
+//  Created by Elena Lucher on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
